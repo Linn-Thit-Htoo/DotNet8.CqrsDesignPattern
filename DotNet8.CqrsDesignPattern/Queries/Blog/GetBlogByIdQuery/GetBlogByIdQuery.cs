fd64442a7352c8851e552a1324e1d4735ac10667
Linn Thit <lth1212001@gmail.com>
2024-05-27 13:19:41 +0630

    -

--- DotNet8.CqrsDesignPattern/Queries/Blog/GetBlogByIdQuery/GetBlogByIdQuery.cs
+++ DotNet8.CqrsDesignPattern/Queries/Blog/GetBlogByIdQuery/GetBlogByIdQuery.cs
@@ -1,10 +1,9 @@
 ﻿using DotNet8.CqrsDesignPattern.Models.Blog;
 using MediatR;
 
-namespace DotNet8.CqrsDesignPattern.Queries.Blog.GetBlogByIdQuery
+namespace DotNet8.CqrsDesignPattern.Queries.Blog.GetBlogByIdQuery;
+
+public class GetBlogByIdQuery : IRequest<BlogModel>
 {
-    public class GetBlogByIdQuery : IRequest<BlogModel>
-    {
-        public long BlogId { get; set; }
-    }
+    public long BlogId { get; set; }
 }
