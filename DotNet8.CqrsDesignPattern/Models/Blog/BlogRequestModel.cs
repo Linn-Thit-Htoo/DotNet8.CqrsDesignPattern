83b48045cf27dd1eba811d7392141fbbeb23dbec
Linn Thit <lth1212001@gmail.com>
2024-05-27 13:18:54 +0630

    -

--- DotNet8.CqrsDesignPattern/Models/Blog/BlogRequestModel.cs
+++ DotNet8.CqrsDesignPattern/Models/Blog/BlogRequestModel.cs
@@ -1,9 +1,8 @@
-﻿namespace DotNet8.CqrsDesignPattern.Models.Blog
+﻿namespace DotNet8.CqrsDesignPattern.Models.Blog;
+
+public class BlogRequestModel
 {
-    public class BlogRequestModel
-    {
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-    }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
