9c922768dca267029dba92f1da1bb7516b6b8933
Linn Thit <lth1212001@gmail.com>
2024-05-27 13:23:02 +0630

    -

--- DotNet8.CqrsDesignPattern/Repositories/Blog/IBlogRepository.cs
+++ DotNet8.CqrsDesignPattern/Repositories/Blog/IBlogRepository.cs
@@ -1,11 +1,10 @@
 ﻿using DotNet8.CqrsDesignPattern.Models.Blog;
 
-namespace DotNet8.CqrsDesignPattern.Repositories.Blog
+namespace DotNet8.CqrsDesignPattern.Repositories.Blog;
+
+public interface IBlogRepository
 {
-    public interface IBlogRepository
-    {
-        Task<BlogListResponseModel> GetBlogsAsync();
-        Task<BlogModel> GetBlogByIdAsync(long id);
-        Task<int> CreateBlogAsync(BlogRequestModel requestModel);
-    }
+    Task<BlogListResponseModel> GetBlogsAsync();
+    Task<BlogModel> GetBlogByIdAsync(long id);
+    Task<int> CreateBlogAsync(BlogRequestModel requestModel);
 }
