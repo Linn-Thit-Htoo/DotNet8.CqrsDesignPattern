4d864a72d249c8e9d3272b0b1c56634ec2a4582a
Linn Thit <lth1212001@gmail.com>
2024-05-27 13:23:41 +0630

    -

--- DotNet8.CqrsDesignPattern/Program.cs
+++ DotNet8.CqrsDesignPattern/Program.cs
@@ -4,13 +4,10 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers().AddJsonOptions(opt =>
 {
     opt.JsonSerializerOptions.PropertyNamingPolicy = null;
 });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -25,7 +22,6 @@ builder.Services.AddScoped<IBlogRepository, BlogRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
