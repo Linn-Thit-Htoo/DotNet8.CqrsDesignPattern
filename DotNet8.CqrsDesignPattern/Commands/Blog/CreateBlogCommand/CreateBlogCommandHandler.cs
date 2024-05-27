2287e17f63a2ab87e5b97900cf113ce7196b7c8e
Linn Thit <lth1212001@gmail.com>
2024-05-27 13:16:54 +0630

    -

--- DotNet8.CqrsDesignPattern/Commands/Blog/CreateBlogCommand/CreateBlogCommandHandler.cs
+++ DotNet8.CqrsDesignPattern/Commands/Blog/CreateBlogCommand/CreateBlogCommandHandler.cs
@@ -1,20 +1,19 @@
 ﻿using DotNet8.CqrsDesignPattern.Repositories.Blog;
 using MediatR;
 
-namespace DotNet8.CqrsDesignPattern.Commands.Blog.CreateBlogCommand
+namespace DotNet8.CqrsDesignPattern.Commands.Blog.CreateBlogCommand;
+
+public class CreateBlogCommandHandler : IRequestHandler<CreateBlogCommand, int>
 {
-    public class CreateBlogCommandHandler : IRequestHandler<CreateBlogCommand, int>
+    private readonly IBlogRepository _blogRepository;
+
+    public CreateBlogCommandHandler(IBlogRepository blogRepository)
     {
-        private readonly IBlogRepository _blogRepository;
+        _blogRepository = blogRepository;
+    }
 
-        public CreateBlogCommandHandler(IBlogRepository blogRepository)
-        {
-            _blogRepository = blogRepository;
-        }
-
-        public async Task<int> Handle(CreateBlogCommand request, CancellationToken cancellationToken)
-        {
-            return await _blogRepository.CreateBlogAsync(request.Blog);
-        }
+    public async Task<int> Handle(CreateBlogCommand request, CancellationToken cancellationToken)
+    {
+        return await _blogRepository.CreateBlogAsync(request.Blog);
     }
 }
